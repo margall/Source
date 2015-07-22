3f5d1d0592463540ae6e6cdce8b4607895d8763f
margall <marekgallus@gmail.com>
2015-07-22 11:33:52 +0200

    SHAFT_PLL_1

--- SH_FREQ_MULTIPLIER.vhd
+++ SH_FREQ_MULTIPLIER.vhd
@@ -47,7 +47,7 @@ signal counter_fback_initial : std_logic_vector (7 downto 0) := (others => '0');
 signal to_compare_count : std_logic_vector (21 downto 0);
 signal mul_freq : std_logic;
 signal update : std_logic;
-signal update_count : std_logic_vector (1 downto 0);
+signal update_count : std_logic_vector (2 downto 0);
 signal edges_counter_sh : std_logic_vector (1 downto 0);
 signal edges_counter_fb : std_logic_vector (1 downto 0);
 signal shaft_counter : std_logic_vector (21 downto 0);
@@ -134,7 +134,9 @@ begin
 			counter_fback <= (others => '0');
 			mul_freq_count <= x"0001"; 
 		elsif (SH_16MHz_CLK'event and SH_16MHz_CLK = '1') then
-			if (mul_freq_count = x"0000") then
+			if (mul_freq_init_load = '1') then     ---
+				mul_freq_count <= x"0001"; 
+			elsif (mul_freq_count = x"0000") then
 				mul_freq <= not mul_freq;
 				if counter_fback = x"00" then
 					counter_fback <= counter_fback_initial;
@@ -143,7 +145,7 @@ begin
 				end if;
 				mul_freq_count <= mul_freq_count_to;
 			elsif (mul_freq_count_to /= x"FFFF") then 
-				mul_freq_count <= mul_freq_count - 1;			
+				mul_freq_count <= mul_freq_count - 1;					   
 			end if;
 		end if;
 	end process;
@@ -184,7 +186,7 @@ begin
 					edges_counter_fb <= (others => '0');
 				elsif (edges_counter_sh /= "00" and edges_counter_fb /= "00") then
 					update <= '1';
-					update_count <= update_count + 1;
+					update_count <= update_count + '1';
 					edges_counter_sh <= "00";
 					edges_counter_fb <= "00";
 				elsif (edges_counter_sh = "00") then
@@ -210,17 +212,19 @@ begin
 			elsif mul_freq_init_load = '1' and EDGE = '1' then
 				mul_freq_init_load <= '0';
 				mul_freq_count_to <= mul_freq_div_out (15 downto 0);
-			elsif update_count = "00" and update <= '1' then
-				if (difference > signed(SH_FREQ_MUL) ) and difference < 64 then
-					if mul_freq_count_to < x"FFFF" then
-						mul_freq_count_to <= mul_freq_count_to - '1';
-					end if;
-				elsif (difference < -(signed(SH_FREQ_MUL))) and difference > -64 then
-					if mul_freq_count_to > x"0000" then
-						mul_freq_count_to <= mul_freq_count_to + '1';
-					end if;
-				elsif difference < 128 or difference > -128 then
-					if (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 5)) < 65535 and (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 5)) > 0 then
+			elsif (update_count = "000" and update = '1') then
+				if difference < 128 or difference > -128 then
+					if (difference < signed(counter_fback_initial) and difference > -(signed(counter_fback_initial))) then
+						-- do nothing
+					elsif (difference > signed(counter_fback_initial) ) and difference < 64 then
+						if mul_freq_count_to < x"FFFF" then
+							mul_freq_count_to <= mul_freq_count_to - '1';
+						end if;
+					elsif (difference < -(signed(counter_fback_initial)) and difference > (-64)) then 
+						if mul_freq_count_to > x"0000" then
+							mul_freq_count_to <= mul_freq_count_to + '1';
+						end if;
+					elsif (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 5)) < 65535 and (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 5)) > 0 then
 						if (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 5)) /= signed(mul_freq_count_to) then
 							mul_freq_count_to <= std_logic_vector(signed(mul_freq_count_to) - shift_right(difference(15 downto 0), 5));
 						elsif difference > 0 then
@@ -230,8 +234,12 @@ begin
 						end if;
 					end if;
 				else
-					if (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) < 65535 and (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) > 0 then
-						mul_freq_count_to <= std_logic_vector(signed(mul_freq_count_to) - shift_right(difference(15 downto 0), 8));
+					--if (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) < 65535 and (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) > 0 then
+						--mul_freq_count_to <= std_logic_vector(signed(mul_freq_count_to) - shift_right(difference(15 downto 0), 8));
+					if (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) > 65535 then
+						mul_freq_count_to <= x"FFFF";
+					elsif (signed(mul_freq_count_to) + shift_right(difference(15 downto 0), 8)) < 0 then
+						mul_freq_count_to <= x"0001";
 					end if;
 				end if;
 			end if;
